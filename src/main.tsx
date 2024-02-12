39b7cd8af23237233ed86e0efa55ff31faaba889
Sabeer Narula <sn23@bu.edu>
2024-02-12 17:52:20 -0500

    removing speed insights because of incompatability

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <App />
       <Analytics />
-      <SpeedInsights />
     </Router>
   </React.StrictMode>
 );
