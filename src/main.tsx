d2f4206427ff624fd01b8bce8b5d46fe03a956c6
Sabeer Narula <sn23@bu.edu>
2024-02-12 17:49:18 -0500

    adding vercel analytics to project

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </Router>
   </React.StrictMode>
 );
